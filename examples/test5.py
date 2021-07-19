7f4598a84ee991d69ea18ba1c14ffc2e41571c3b
Malik Enes Safak <e.maliksafak@gmail.com>
2021-07-19 20:11:59 +0300

    Examples: Something insignificant

--- examples/test5.py
+++ examples/test5.py
@@ -1,5 +1,5 @@
 from PyxelWidgets.Controller.MIDI.Launchpad.MK3 import *
-from PyxelWidgets.Widgets.Extra.Keyboard import *
+from PyxelWidgets.Widgets.Fader import *
 from PyxelWidgets.Window import *
 from PyxelWidgets.WindowManager import *
 import time
@@ -7,18 +7,18 @@ import time
 controller = MK3('Input', 'Input', Model.Mini)
 controller2 = MK3('Output', 'Output', Model.Mini)
 
-keyboard = Keyboard('keyboard', 0, 0, 8, 16)
+fader = Fader('fader', 0, 0, 20, 10)
 
-window = Window('mainWindow', 8, 16)
-window.addWidgets(keyboard.widgets)
+window = Window('mainWindow', 20, 10)
+window.addWidget(fader)
 
 manager = WindowManager()
 
 manager.addController(controller, 0, 0)
-manager.addController(controller2, 0, 10)
+manager.addController(controller2, 10, 0)
 
 manager.addWindow(window)
-manager.addWindowToRenderer('mainWindow', 1, 1, 8, 16)
+manager.addWindowToRenderer('mainWindow', 1, 1, 20, 10)
 
 if __name__ == "__main__":
     manager.run()
