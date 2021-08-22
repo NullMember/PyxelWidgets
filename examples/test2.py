fa721ad1341068ce22258770c4024459b1543bf3
Malik Enes Safak <e.maliksafak@gmail.com>
2021-08-22 14:49:04 +0300

    These should not be in here

--- examples/test2.py
+++ examples/test2.py
@@ -2,59 +2,38 @@ from PyxelWidgets.Window import *
 from PyxelWidgets.Widgets import *
 from PyxelWidgets.Widgets.Extra import *
 from PyxelWidgets.Controller.MIDI.Launchpad.MK3 import *
+from PyxelWidgets.Util.Clock import *
 import time
 
-def cb(name: str, value):
-    if 'bg' in name:
-        name, x, y = name.split(',')
-        if value > 0:
-            if x == 0:
-                ws2.tone = Keyboard.KeyboardTone.C
-                ws2.octave = 0
-            elif x == 1:
-                ws2.tone = Keyboard.KeyboardTone.D
-                ws2.octave = 0
-            elif x == 2:
-                ws2.tone = Keyboard.KeyboardTone.E
-                ws2.octave = 0
-            elif x == 3:
-                ws2.tone = Keyboard.KeyboardTone.F
-                ws2.octave = 0
-            elif x == 4:
-                ws2.tone = Keyboard.KeyboardTone.G
-                ws2.octave = 0
-            elif x == 5:
-                ws2.tone = Keyboard.KeyboardTone.A
-                ws2.octave = 0
-            elif x == 6:
-                ws2.tone = Keyboard.KeyboardTone.B
-                ws2.octave = 0
-            elif x == 7:
-                ws2.tone = Keyboard.KeyboardTone.C
-                ws2.octave = 1
-    print(name, value)
+def cb(name, event, value):
+    print(name, event, value)
 
-w = Window('w', 20, 10)
-ws = [Button.Button('b1', 1, 5, 4, 4, callback = cb, mode = Button.ButtonMode.Switch),
-      Fader.Fader('f1', 1, 1, 4, 4, callback = cb, grid = Fader.FaderGrid.Matrix, resolution = 8),
-      Sequencer.Sequencer('s1', 5, 5, 4, 4, callback = cb),
-      XY.XY('xy1', 5, 1, 4, 4, callback = cb)]
-ws2 = Keyboard.Keyboard('keyboard', 1, 1, 8, 8, mode = Keyboard.KeyboardMode.DiatonicVertical, callback = cb)
-wb = ButtonGroup.ButtonGroup('bg', 1, 9, 8, 1, callback = cb)
+w = Window('w', 10, 10)
+#ws2 = Keyboard.Keyboard('keyboard', 1, 1, 8, 8, mode = Keyboard.KeyboardMode.DiatonicVertical, callback = cb)
+#wb = ButtonGroup.ButtonGroup('bg', 1, 9, 8, 1, callback = cb)
 
-c = MK3('Input', 'Input', Model.Mini)
-w.addWidgets(ws2.widgets)
-w.addWidgets(wb.widgets)
-w.setCallback(c.updateArea)
-c.setCallback(w.setButton)
-c.init()
+c = MK3('MIDIIN2', 'MIDIOUT2', Model.Mini)
+w.addWidget(Button.Button('b1', 4, 4, callback = cb, mode = Button.ButtonMode.Switch), 1, 5)
+# w.addWidget(Knob.Knob('k1', 4, 4, callback = cb), 1, 1)
+w.addWidget(Fader.Fader('f1', 4, 4, callback = cb, grid = Fader.FaderGrid.Matrix, resolution = 8), 1, 1)
+w.addWidget(Sequencer.Sequencer('s1', 4, 4, callback = cb), 5, 5)
+w.addWidget(XY.XY('xy1', 4, 4, callback = cb), 5, 1)
+#w.addWidgets(wb.widgets)
+c.setCallback(w.process)
+c.connect()
+
+cl = Clock()
+# cl.addTarget(w.getWidget('k1')['widget'].target)
+
+# print()
 
 if __name__ == "__main__":
-    w.run()
+    cl.start()
     while True:
         try:
-            time.sleep(1)
+            c.update(w.x, w.y, w.update())
+            time.sleep(1 / 60)
         except:
-            c.deinit()
-            w.stop()
+            c.disconnect()
+            cl.terminate()
             break
