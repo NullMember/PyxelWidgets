d1723059e07fcd3c44130dba8d0df33eadb871a1
Malik Enes Safak <e.maliksafak@gmail.com>
2021-08-27 12:51:52 +0300

    Sequencer: Fix tick calculation Do not generate repeated callback calls Use updated variable in update method

--- src/PyxelWidgets/Widgets/Sequencer.py
+++ src/PyxelWidgets/Widgets/Sequencer.py
@@ -1,93 +1,84 @@
 from .Widget import Widget
-from threading import Thread
-import time
+from ..Util.Clock import *
 
 class Sequencer(Widget):
-    def __init__(self, name: str, width: int, height: int, **kwargs):
+    def __init__(self, name: str, width: int, height: int, clock: Clock = None, **kwargs):
         super().__init__(name, width, height, **kwargs)
         self._active = [[False for y in range(self.height)] for x in range(self.width)]
-        self._bpm = kwargs.get('bpm', 60)
-        self._numerator = kwargs.get('numerator', 1)
-        self._denominator = kwargs.get('denominator', 4)
-        self._duration = kwargs.get('duration', 4)
+        self._numerator = kwargs.get('numerator', 1.0)
+        self._denominator = kwargs.get('denominator', 4.0)
+        self._ppq = kwargs.get('ppq', 24.0)
         self._currentColor = kwargs.get('currentColor', [0, 255, 0])
-        self._tickPeriod = self._calcTickPeriod()
         self._tickTarget = self.width * self.height
         self._tick = 0
-
-    @property
-    def bpm(self):
-        return self._bpm
-    
-    @bpm.setter
-    def bpm(self, bpm):
-        self._bpm = bpm
-        self._tickPeriod = self._calcTickPeriod()
+        self._target = Target(self.name, self.tick)
+        if clock != None:
+            clock.addTarget(self._target)
     
     @property
     def numerator(self):
         return self._numerator
     
     @numerator.setter
-    def numerator(self, numerator):
+    def numerator(self, numerator: int):
         self._numerator = numerator
-        self._tickPeriod = self._calcTickPeriod()
 
     @property
     def denominator(self):
         return self._denominator
     
     @denominator.setter
-    def denominator(self, denominator):
+    def denominator(self, denominator: int):
         self._denominator = denominator
-        self._tickPeriod = self._calcTickPeriod()
+    
+    @property
+    def ppq(self):
+        return self._ppq
+
+    @ppq.setter
+    def ppq(self, ppq: int):
+        self._ppq = ppq
 
     @property
-    def duration(self):
-        return self._duration
-    
-    @duration.setter
-    def duration(self, duration):
-        self._duration = duration
-        self._tickPeriod = self._calcTickPeriod()
-    
-    @property
-    def period(self):
-        return self._tickPeriod
-    
-    def stop(self):
-        self._tick = 0
+    def target(self):
+        return self._target
 
     def tick(self, tick):
-        self._tick = tick % self._tickTarget
-        if self._isTickActive():
-            self._callback(self.name, self._tick)
+        oldTick = self._tick
+        self._tick = tick / (self._ppq * (self._numerator / self._denominator))
+        self._tick %= self._tickTarget
+        if int(oldTick) != int(self._tick):
+            self._updated = True
+            self._callback(self.name, 'tick', int(self._tick))
+            if self._isTickActive():
+                self._callback(self.name, 'active', int(self._tick))
 
     def pressed(self, x: int, y: int, value: float):
         self._active[x][y] = not self._active[x][y]
+        self._updated = True
         super().pressed(x, y, value)
 
     def update(self) -> list:
-        for x in range(self.width):
-            for y in range(self.height):
-                if self._active[x][y]:
-                    self._pixels[x][y] = self._activeColor
-                else:
-                    self._pixels[x][y] = self._deactiveColor
-        self._pixels[self._tickX()][self._tickY()] = self._currentColor
-        return self._pixels
-
-    def _calcTickPeriod(self):
-        return (60.0 / self.bpm) / ((self.numerator / self.denominator) * 4.0) / self.duration
+        if self._updated:
+            self._updated = False
+            for x in range(self.width):
+                for y in range(self.height):
+                    if self._active[x][y]:
+                        self._pixels[x][y] = self._activeColor
+                    else:
+                        self._pixels[x][y] = self._deactiveColor
+            self._pixels[self._tickX()][self._tickY()] = self._currentColor
+            return self._pixels
+        return []
     
     def _calcTickPosition(self, x, y):
         return x + ((self.height - y - 1) * self.width)
     
     def _tickX(self):
-        return self._tick % self.width
+        return int(self._tick) % self.width
     
     def _tickY(self):
-        return self.height - (self._tick // self.width) - 1
+        return self.height - (int(self._tick) // self.width) - 1
     
     def _isTickActive(self):
         return self._active[self._tickX()][self._tickY()]
