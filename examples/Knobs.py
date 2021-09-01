badd330b8424913e443a46e91d23bc913e5ad360
Malik Enes Safak <e.maliksafak@gmail.com>
2021-09-01 11:17:22 +0300

    Examples: Experimental changes

--- examples/Knobs.py
+++ examples/Knobs.py
@@ -1,68 +1,29 @@
-from PyxelWidgets.Window import *
-from PyxelWidgets.Widgets import *
-from PyxelWidgets.Widgets.Extra import *
-from PyxelWidgets.Controller.MIDI.Launchpad.MK3 import *
-from PyxelWidgets.Util.Clock import *
-from DAWController import *
+from PyxelWidgets import Window
+from PyxelWidgets.Widgets import Knob
+from PyxelWidgets.Controller.MIDI.Launchpad import MK3
+from PyxelWidgets.Util import Clock
 import time
 from random import randint
 
 def controllerCallback(name, event, value):
     print(name, event, value)
-    if name[0] == 'k':
-        if name == 'k00':
-            daw.vpot(0, int(window.widgets[name]['widget'].delta * 127))
-        if name == 'k10':
-            daw.vpot(1, int(window.widgets[name]['widget'].delta * 127))
-        if name == 'k01':
-            daw.vpot(2, int(window.widgets[name]['widget'].delta * 127))
-        if name == 'k11':
-            daw.vpot(3, int(window.widgets[name]['widget'].delta * 127))
-        if name == 'b00':
-            daw.tap(MCUButton.VSELECT1)
-        if name == 'b10':
-            daw.tap(MCUButton.VSELECT2)
-        if name == 'b01':
-            daw.tap(MCUButton.VSELECT3)
-        if name == 'b11':
-            daw.tap(MCUButton.VSELECT4)
     # pass
 
-def dawCallback(name, data):
-    print(name, data)
-    if name == 'vpot':
-        index, mode, ring, center = data
-        if index == 0:
-            window.widgets['k00']['widget'].display = ring / 12.0
-            window.widgets['b00']['widget'].value = center
-        if index == 1:
-            window.widgets['k10']['widget'].display = ring / 12.0
-            window.widgets['b10']['widget'].value = center
-        if index == 2:
-            window.widgets['k01']['widget'].display = ring / 12.0
-            window.widgets['b01']['widget'].value = center
-        if index == 3:
-            window.widgets['k11']['widget'].display = ring / 12.0
-            window.widgets['b11']['widget'].value = center
-
-window = Window('w', 10, 10)
-controller = MK3('Input', 'Input', Model.Mini)
-clock = Clock()
-daw = DAWController()
-daw.connect('DAWO', 'DAWI')
-daw.setCallback(dawCallback)
+window = Window.Window(10, 10)
+controller = MK3.MK3('MIDIIN2', 'MIDIOUT2', MK3.Model.Mini)
+clock = Clock.Clock()
 
 for x in range(2):
     for y in range(2):
-        window.addWidget(Knob.Knob('k' + str(x) + str(y), 4, 4, callback = controllerCallback, clock = clock, activeColor = [randint(0, 255), randint(0, 255), randint(0, 255)]), (x * 4) + 1, (y * 4) + 1)
-        window.addWidget(Button.Button('b' + str(x) + str(y), 2, 2, callback = controllerCallback, activeColor = [randint(0, 255), randint(0, 255), randint(0, 255)]), (x * 4) + 2, (y * 4) + 2)
+        window.addWidget(Knob.Knob(4, 4, callback = controllerCallback, clock = clock, type = Knob.KnobType.BoostCut, activeColor = [randint(0, 255), randint(0, 255), randint(0, 255)]), (x * 4) + 1, (y * 4) + 1)
+        window.addWidget(Knob.Knob(2, 2, callback = controllerCallback, clock = clock, type = Knob.KnobType.BoostCut, activeColor = [randint(0, 255), randint(0, 255), randint(0, 255)]), (x * 4) + 2, (y * 4) + 2)
 
 # for x in range(4):
 #     window.addWidget(Knob.Knob('k' + str(x), 1, 8, callback = controllerCallback, clock = clock, activeColor = [randint(0, 255), randint(0, 255), randint(0, 255)]), 1 + x, 1)
 
 controller.setCallback(window.process)
 controller.connect()
-clock.addTarget(Target('update', lambda tick : controller.update(window.x, window.y, window.update())))
+clock.addTarget(Clock.Target('update', lambda tick : controller.update(window.x, window.y, window.update())))
 
 if __name__ == "__main__":
     clock.start()
--- examples/PolySeq.py
+++ examples/PolySeq.py
@@ -3,9 +3,10 @@ from PyxelWidgets.Controller.MIDI.Launchpad.MK3 import *
 from PyxelWidgets.Widgets.Sequencer import *
 from PyxelWidgets.Util.Clock import *
 
-controller = MK3('MIDI 2', 'MIDI 2', Model.Mini)
+controller = MK3('Input', 'Input', Model.Mini)
 window = Window('main', 10, 10)
-clock = Clock(240, 960)
+clock = Clock(120, 480)
+controller.setCallback(window.process)
 
 def cb(name, event, value):
     pass
@@ -13,14 +14,15 @@ def cb(name, event, value):
 for i in range(8):
     window.addWidget(Sequencer('s' + str(i), 8, 1, clock = clock, callback = cb), 1, 1 + i)
 
-clock.addTarget(Target('update', controller.update(window.x, window.y, window.update())))
+# clock.addTarget(Target('update', lambda tick : controller.update(window.x, window.y, window.update())))
 
 if __name__ == "__main__":
     controller.connect()
     clock.start()
     while True:
         try:
-            time.sleep(1)
+            controller.update(window.x, window.y, window.update())
+            time.sleep(1 / 60)
         except:
             clock.terminate()
             controller.disconnect()
