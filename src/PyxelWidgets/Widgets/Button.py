32edeebfbe917ad78485ce218ef1291e438aa80d
Malik Enes Safak <e.maliksafak@gmail.com>
2021-09-09 13:46:20 +0300

    Button: Remove unnecessery property attributes Make most of the private variables to public Rename held variable to hold

--- src/PyxelWidgets/Widgets/Button.py
+++ src/PyxelWidgets/Widgets/Button.py
@@ -10,43 +10,43 @@ class Button(Widget):
     def __init__(self, width: int, height: int, **kwargs):
         name = kwargs.get('name', 'Button_' + str(Button._count))
         super().__init__(name, width, height, **kwargs)
-        self._mode = kwargs.get('mode', ButtonMode.Button)
-        self._held = False
-        self._state = False
+        self.mode = kwargs.get('mode', ButtonMode.Button)
+        self.hold = False
+        self.state = False
         Button._count += 1
 
     def pressed(self, x: int, y: int, value: float):
-        if self._mode == ButtonMode.Button:
+        if self.mode == ButtonMode.Button:
             self.value = value
-        elif self._mode == ButtonMode.Switch:
-            if self._state:
+        elif self.mode == ButtonMode.Switch:
+            if self.state:
                 self.value = 0.0
-                self._state = False
+                self.state = False
             else:
                 self.value = value
-                self._state = True
-        elif self._mode == ButtonMode.Mixed:
-            if self._held:
+                self.state = True
+        elif self.mode == ButtonMode.Mixed:
+            if self.hold:
                 return
             else:
                 self.value = value
         super().pressed(x, y, self.value)
     
     def released(self, x: int, y: int, value: float):
-        if self._mode == ButtonMode.Button:
+        if self.mode == ButtonMode.Button:
             self.value = 0.0
-        elif self._mode == ButtonMode.Switch:
+        elif self.mode == ButtonMode.Switch:
             pass
-        elif self._mode == ButtonMode.Mixed:
-            if self._held:
+        elif self.mode == ButtonMode.Mixed:
+            if self.hold:
                 return
             else:
                 self.value = 0.0
         super().released(x, y, self.value)
     
     def held(self, x: int, y: int, value: float):
-        if self._mode == ButtonMode.Mixed:
-            self._held = not self._held
+        if self.mode == ButtonMode.Mixed:
+            self.hold = not self.hold
         super().held(x, y, self.value)
 
     def updateArea(self, sx, sy, sw, sh):
