1f755a2df825a3207a29336c5c1c2c3cae1f55a9
Malik Enes Safak <e.maliksafak@gmail.com>
2021-09-11 00:05:56 +0300

    Sine: Use EffectDirection.Up as default movement Return scaled output from step

--- src/PyxelWidgets/Util/Effects/Sine.py
+++ src/PyxelWidgets/Util/Effects/Sine.py
@@ -1,10 +1,11 @@
-from .Effect import Effect
+from .Effect import *
 from math import pi, sin
 
 class Sine(Effect):
     def __init__(self, **kwargs) -> None:
+        kwargs['direction'] = kwargs.get('direction', EffectDirection.Up)
         super().__init__(**kwargs)
     
     def step(self):
         super().step()
-        return sin((self._cycleCurrent / self._cycleTotal) * pi)
+        return (sin((self._cycleCurrent / self._cycleTotal) * 2 * pi) + 1) / 2
