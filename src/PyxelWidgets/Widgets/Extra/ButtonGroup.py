900a82b98dbf26f169ab2ae80c2fbd1168aacb3e
Malik Enes Şafak <e.maliksafak@gmail.com>
2021-10-12 09:22:50 +0300

    ButtonGroup: Rewrite

--- src/PyxelWidgets/Widgets/Extra/ButtonGroup.py
+++ src/PyxelWidgets/Widgets/Extra/ButtonGroup.py
@@ -1,53 +1,17 @@
 from ..Button import Button
+from ...Helpers import *
+import numpy
 
 class ButtonGroup():
-    def __init__(self, name: str, width: int, height: int, **kwargs):
-        self.width = width
-        self.height = height
-        self.name = name
-        self._buttons = [[Button(name + ',' + str(_x) + ',' + str(_y), \
-                                1, \
-                                1, \
-                                **kwargs) \
-                                for _y in range(height)] \
-                                for _x in range(width)] 
-        self._hold = kwargs.get('hold', True)
-        self._deactiveColor = self._buttons[0][0].deactiveColor
-        self._activeColor = self._buttons[0][0].activeColor
-        self._lastButton = [-1, -1]
-    
-    @property
-    def buttons(self):
-        return self._buttons
-    
-    @property
-    def widgets(self):
-        return [y for x in self.buttons for y in x]
-    
-    @property
-    def width(self):
-        return self._width
-    
-    @width.setter
-    def width(self, width: int):
-        self._width = width
-    
-    @property
-    def height(self):
-        return self._height
-    
-    @height.setter
-    def height(self, height: int):
-        self._height = height
-    
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self, name: str):
-        self._name = name
-    
-    def setCallback(self, callback):
-        for widget in self.widgets:
-            widget.setCallback(callback)
+
+    _count = 0
+
+    def __init__(self, x: int, y: int, width: int, height: int, **kwargs):
+        self.name = kwargs.get('name', f'ButtonGroup_{ButtonGroup._count}')
+        if 'name' in kwargs.keys():
+            del kwargs['name']
+        self.rect = Rectangle2D(x, y, width, height)
+        self.buttons = numpy.array([[Button(self.rect.x + _x, self.rect.y + _y, 1, 1, \
+                                    name = f'{self.name}_{_x}_{_y}', **kwargs) \
+                                    for _y in range(height)] for _x in range(width)])
+        ButtonGroup._count += 1
