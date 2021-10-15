2ef9d3a217561426684bf23bc51c7b7609470a73
Malik Enes Şafak <e.maliksafak@gmail.com>
2021-10-15 20:44:07 +0300

    Launchpad: Add MK1, MK2 and Pro support Move Enums to classes

--- src/PyxelWidgets/Controller/MIDI/Launchpad/Launchpad.py
+++ src/PyxelWidgets/Controller/MIDI/Launchpad/Launchpad.py
@@ -1,5 +1,8 @@
 from ..MIDI import MIDI
 
 class Launchpad(MIDI):
+    """
+    Base class for Novation's Launchpad devices
+    """
     def __init__(self, inPort: str = None, outPort: str = None, **kwargs):
         super().__init__(inPort, outPort, **kwargs)
