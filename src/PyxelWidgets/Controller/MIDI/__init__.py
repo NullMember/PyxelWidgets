41b24a026511faf2854268566cbccc2e62bb464d
Malik Enes Şafak <e.maliksafak@gmail.com>
2021-10-22 23:09:27 +0300

    MIDI: Implement InvalidMIDIPortException Raise InvalidMIDIPortException when MIDI port is invalid Add listInputDevices, listOutputDevices and listDevices static methods Do not connect if already connected Do not disconnect if already disconnected

--- src/PyxelWidgets/Controller/MIDI/__init__.py
+++ src/PyxelWidgets/Controller/MIDI/__init__.py
@@ -3,25 +3,47 @@ __all__ = ['Akai', 'Novation', 'Presonus']
 import PyxelWidgets.Controller
 import rtmidi.midiutil
 
+class InvalidMIDIPortException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__("MIDI Port Invalid", *args)
+
 class MIDI(PyxelWidgets.Controller.Controller):
     def __init__(self, inPort: str, outPort: str, **kwargs):
         super().__init__(**kwargs)
-        self._midiInput, self._midiInputName = rtmidi.midiutil.open_midiinput(inPort, interactive = False)
-        self._midiOutput, self._midiOutputName = rtmidi.midiutil.open_midioutput(outPort, interactive = False)
-        self._midiInput.ignore_types(sysex = False)
+        try:
+            self._midiInput, self._midiInputName = rtmidi.midiutil.open_midiinput(inPort, interactive = False)
+            self._midiOutput, self._midiOutputName = rtmidi.midiutil.open_midioutput(outPort, interactive = False)
+            self._midiInput.ignore_types(sysex = False)
+        except:
+            raise InvalidMIDIPortException()
     
     def __del__(self):
         self.disconnect()
         self._midiInput.close_port()
         self._midiOutput.close_port()
 
+    @staticmethod
+    def listInputDevices():
+        rtmidi.midiutil.list_input_ports()
+    
+    @staticmethod
+    def listOutputDevices():
+        rtmidi.midiutil.list_output_ports()
+    
+    @staticmethod
+    def listIODevices():
+        MIDI.listInputDevices()
+        MIDI.listOutputDevices()
+
     def connect(self):
-        super().connect()
+        if not self.connected:
+            super().connect()
         self._midiInput.set_callback(self.processMIDI)
     
     def disconnect(self):
         self._midiInput.set_callback(lambda *_, **__: None)
-        super().disconnect()
+        if self.connected:
+            super().disconnect()
     
     def processMIDI(self, message, _):
         raise NotImplementedError("processMIDI must be implemented")
