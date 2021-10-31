fda08e70fd66713f3843362bdebf6c1e707245f6
Malik Enes Şafak <e.maliksafak@gmail.com>
2021-10-31 15:47:06 +0300

    REAPER: Add experimental REAPER integration (not completed)

---
+++ src/PyxelWidgets/Controller/OSC/REAPER/__init__.py
@@ -0,0 +1,16 @@
+import PyxelWidgets.Controller.OSC
+
+class REAPER(PyxelWidgets.Controller.OSC.OSC):
+    def __init__(self, ip, in_port=8000, out_port=9000, **kwargs):
+        super().__init__(ip, in_port=in_port, out_port=out_port, **kwargs)
+        self.messages = {
+            "Update" : "/update",
+            "Crossfade" : "/crossfade",
+            "ProjectName" : "/project/name",
+            "ProjectEngine" : "/project/engine",
+            "Play" : "/play",
+            "Record" : "/record",
+            "Repeat" : "/repeat",
+            "Click" : "/click",
+            "ClickVolume" : "/click/volume"
+        }
