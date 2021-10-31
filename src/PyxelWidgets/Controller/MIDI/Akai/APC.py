d7f3c5e55d09aa8e10d0a753201ddec77b792215
Malik Enes Şafak <e.maliksafak@gmail.com>
2021-10-31 15:36:51 +0300

    APCMini: Add custom controls

--- src/PyxelWidgets/Controller/MIDI/Akai/APC.py
+++ src/PyxelWidgets/Controller/MIDI/Akai/APC.py
@@ -3,7 +3,14 @@ import PyxelWidgets.Helpers
 import enum
 
 class Mini(PyxelWidgets.Controller.MIDI.MIDI):
+    """
+    Akai APC Mini Controller class
+    ------------------------------
 
+    Thanks to TomasHubelbauer for his amazing documentation:
+        https://github.com/TomasHubelbauer/akai-apc-mini
+        
+    """
     class Colors(enum.Enum):
         Off = 0x00
         Green = 0x01
@@ -12,6 +19,36 @@ class Mini(PyxelWidgets.Controller.MIDI.MIDI):
         RedBlink = 0x04
         Yellow = 0x05
         YellowBlink = 0x06
+    
+    class Notes(enum.Enum):
+        Button_1 = 64
+        Button_2 = 65
+        Button_3 = 66
+        Button_4 = 67
+        Button_5 = 68
+        Button_6 = 69
+        Button_7 = 70
+        Button_8 = 71
+        Button_9 = 98
+        Button_10 = 89
+        Button_11 = 88
+        Button_12 = 87
+        Button_13 = 86
+        Button_14 = 85
+        Button_15 = 84
+        Button_16 = 83
+        Button_17 = 82
+    
+    class Controls(enum.Enum):
+        Fader_1 = 48
+        Fader_2 = 49
+        Fader_3 = 50
+        Fader_4 = 51
+        Fader_5 = 52
+        Fader_6 = 53
+        Fader_7 = 54
+        Fader_8 = 55
+        Fader_9 = 56
 
     def __init__(self, inPort: str, outPort: str, **kwargs):
         kwargs['width'] = kwargs.get('width', 8)
@@ -35,20 +72,16 @@ class Mini(PyxelWidgets.Controller.MIDI.MIDI):
         if cmd == 0x80:
             if midi[1] < 64:
                 x = midi[1] % 8
-                y = midi[1] // 6
+                y = midi[1] // 8
                 self.setButton(x, y, 0.0)
-            elif midi[1] < 72:
-                self.setCustom('released', 'button', midi[1] - 62, 0, 0.0)
-            elif midi[1] < 90:
-                self.setCustom('released', 'button', 0, 8 - (midi[1] - 82), 0.0)
+            else:
+                self.setCustom('released', Mini.Notes(midi[1]).name, 0.0)
         elif cmd == 0x90:
             if midi[1] < 64:
                 x = midi[1] % 8
                 y = midi[1] // 8
                 self.setButton(x, y, 1.0)
-            elif midi[1] < 72:
-                self.setCustom('pressed', 'button', midi[1] - 62, 0, 1.0)
-            elif midi[1] < 90:
-                self.setCustom('pressed', 'button', 0, 8 - (midi[1] - 82), 1.0)
+            else:
+                self.setCustom('pressed', Mini.Notes(midi[1]).name, 1.0)
         elif cmd == 0xB0:
-            self.setCustom('changed', 'slider', midi[1] - 48, 0, midi[2] / 127.0)
+            self.setCustom('changed', Mini.Controls(midi[1]).name, midi[2] / 127.0)
