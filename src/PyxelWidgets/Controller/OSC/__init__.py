b879ac664334c79ab14a848809042905db840cf6
Malik Enes Şafak <e.maliksafak@gmail.com>
2021-10-31 15:36:33 +0300

    OSC: Move base OSC class to init OSC class rewritten (not completed yet)

---
+++ src/PyxelWidgets/Controller/OSC/__init__.py
@@ -0,0 +1,44 @@
+__all__ = ['REAPER']
+
+import pythonosc.udp_client
+import pythonosc.dispatcher
+import pythonosc.osc_server
+import pythonosc.osc_bundle_builder
+import pythonosc.osc_message_builder
+import threading
+import PyxelWidgets.Controller
+
+class OSC(PyxelWidgets.Controller.Controller):
+    def __init__(self, ip, in_port = 8000, out_port = 9000, **kwargs):
+        super().__init__(**kwargs)
+        self.client = pythonosc.udp_client.SimpleUDPClient(ip, out_port)
+        self.dispatcher = pythonosc.dispatcher.Dispatcher()
+        self.server = pythonosc.osc_server.ThreadingOSCUDPServer(('0.0.0.0', in_port), self.dispatcher)
+        self.messages = {}
+        self.handlers = {}
+        self.responses = {}
+
+    def connect(self):
+        super().connect()
+        self.register()
+        self._serverThread = threading.Thread(target = self.server.serve_forever)
+        self._serverThread.start()
+    
+    def disconnect(self):
+        self.server.shutdown()
+        self.unregister()
+        super().disconnect()
+    
+    def send(self, name, value):
+        self.client.send_message(self.messages[name], value)
+    
+    def register(self):
+        for message in self.messages:
+            self.handlers[message] = self.dispatcher.map(self.messages[message], self.process, message)
+    
+    def unregister(self):
+        for message in self.messages:
+            self.dispatcher.unmap(self.messages[message], self.handlers[message])
+    
+    def process(self, addr: str, args, value):
+        print(args[0], addr, value)
