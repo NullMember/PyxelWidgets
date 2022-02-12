eb71f443b2a4dce8299c2901fb176654ed0c16bf
Malik Enes Şafak <e.maliksafak@gmail.com>
2022-02-12 15:50:07 +0300

    Effect: bugfix

--- src/PyxelWidgets/Utils/Effect/__init__.py
+++ src/PyxelWidgets/Utils/Effect/__init__.py
@@ -13,13 +13,11 @@ class Effect():
 
     def __init__(self, **kwargs) -> None:
         self.name = kwargs.get('name', f'Effect_{Effect._count}')
-        self._length = kwargs.get('length', 30)
-        self.value = 0
-        self.values = [0] * self._length
-        self.length = self._length
-        self.direction = kwargs.get('direction', Effect.Direction.Cycle)
+        self.values = []
+        self.direction = kwargs.get('direction', Effect.Direction.Up)
         self._directionCurrent = 0
         self._cycleCurrent = 0
+        self.length = kwargs.get('length', 30)
         self.reset()
     
     @property
@@ -50,7 +48,7 @@ class Effect():
         elif self.direction == Effect.Direction.Cycle:
             if self._directionCurrent == 0:
                 self._cycleCurrent += 1
-                if self._cycleCurrent == self.length:
+                if self._cycleCurrent == self.length - 1:
                     self._directionCurrent = 1
             elif self._directionCurrent == 1:
                 self._cycleCurrent -= 1
@@ -64,28 +62,27 @@ class Effect():
 class Gaussian(Effect):
     def __init__(self, gauss = 0.25, **kwargs) -> None:
         kwargs['name'] = kwargs.get('name', f'Gaussian_{Gaussian._count}')
-        super().__init__(**kwargs)
         self._gauss = gauss
+        super().__init__(**kwargs)
         Gaussian._count += 1
 
     def calcValues(self):
-        self.reset()
         self.values = [0] * self._length
+        halflen = int(self._length // 2)
         for i in range(self._length):
-            self.values[i] = math.exp(-(pow(self.step() / self.length, 2) / (2 * math.pow(self._gauss, 2))))
+            self.values[i] = math.exp(-(math.pow((i - halflen) / halflen, 2) / (2 * math.pow(self._gauss, 2))))
 
 class Pulse(Effect):
     def __init__(self, width = 0.5, **kwargs) -> None:
         kwargs['name'] = kwargs.get('name', f'Pulse_{Pulse._count}')
-        super().__init__(**kwargs)
         self.width = width
+        super().__init__(**kwargs)
         Pulse._count += 1
     
     def calcValues(self):
-        self.reset()
         self.values = [0] * self._length
         for i in range(self._length):
-            if self.step() < int(self.length * self.width):
+            if i <= int(self._length * self.width):
                 self.values[i] = 1.0
             else:
                 self.values[i] = 0.0
@@ -93,24 +90,21 @@ class Pulse(Effect):
 class Sine(Effect):
     def __init__(self, **kwargs) -> None:
         kwargs['name'] = kwargs.get('name', f'Sine_{Sine._count}')
-        kwargs['direction'] = kwargs.get('direction', Sine.Direction.Up)
         super().__init__(**kwargs)
         Sine._count += 1
     
     def calcValues(self):
-        self.reset()
         self.values = [0] * self._length
         for i in range(self._length):
-            self.values[i] = (math.sin((self.step() / self.length) * 2 * math.pi) + 1) / 2
+            self.values[i] = (math.sin((i / self._length) * 2 * math.pi) + 1) / 2
 
-class Triangle(Effect):
+class Saw(Effect):
     def __init__(self, **kwargs) -> None:
-        kwargs['name'] = kwargs.get('name', f'Triangle_{Triangle._count}')
+        kwargs['name'] = kwargs.get('name', f'Saw_{Saw._count}')
         super().__init__(**kwargs)
-        Triangle._count += 1
+        Saw._count += 1
     
     def calcValues(self):
-        self.reset()
         self.values = [0] * self._length
         for i in range(self._length):
-            self.values[i] = self.step() / self.length
+            self.values[i] = i / self._length
