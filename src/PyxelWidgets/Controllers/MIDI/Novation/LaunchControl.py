9886f5b9af77c96c7b3ced7dcebe033c072a065d
Malik Enes Şafak <e.maliksafak@gmail.com>
2022-03-10 10:36:38 +0300

    LaunchControl XL: Working state

--- src/PyxelWidgets/Controllers/MIDI/Novation/LaunchControl.py
+++ src/PyxelWidgets/Controllers/MIDI/Novation/LaunchControl.py
@@ -7,22 +7,22 @@ import numpy
 class MK1(PyxelWidgets.Controllers.MIDI.MIDI):
 
     class Controls(enum.Enum):
-        Encoder_1 = 0
-        Encoder_2 = 1
-        Encoder_3 = 2
-        Encoder_4 = 3
-        Encoder_5 = 4
-        Encoder_6 = 5
-        Encoder_7 = 6
-        Encoder_8 = 7
-        Encoder_9 = 8
-        Encoder_10 = 9
-        Encoder_11 = 10
-        Encoder_12 = 11
-        Encoder_13 = 12
-        Encoder_14 = 13
-        Encoder_15 = 14
-        Encoder_16 = 15
+        Knob_1 = 0
+        Knob_2 = 1
+        Knob_3 = 2
+        Knob_4 = 3
+        Knob_5 = 4
+        Knob_6 = 5
+        Knob_7 = 6
+        Knob_8 = 7
+        Knob_9 = 8
+        Knob_10 = 9
+        Knob_11 = 10
+        Knob_12 = 11
+        Knob_13 = 12
+        Knob_14 = 13
+        Knob_15 = 14
+        Knob_16 = 15
         Up = 16
         Down = 17
         Left = 18
@@ -97,30 +97,30 @@ class MK1(PyxelWidgets.Controllers.MIDI.MIDI):
 class XL(PyxelWidgets.Controllers.MIDI.MIDI):
 
     class Controls(enum.Enum):
-        Encoder_0 = 13
-        Encoder_1 = 14
-        Encoder_2 = 15
-        Encoder_3 = 16
-        Encoder_4 = 17
-        Encoder_5 = 18
-        Encoder_6 = 19
-        Encoder_7 = 20
-        Encoder_8 = 29
-        Encoder_9 = 30
-        Encoder_10 = 31
-        Encoder_11 = 32
-        Encoder_12 = 33
-        Encoder_13 = 34
-        Encoder_14 = 35
-        Encoder_15 = 36
-        Encoder_16 = 49
-        Encoder_17 = 50
-        Encoder_18 = 51
-        Encoder_19 = 52
-        Encoder_20 = 53
-        Encoder_21 = 54
-        Encoder_22 = 55
-        Encoder_23 = 56
+        Knob_0 = 49
+        Knob_1 = 50
+        Knob_2 = 51
+        Knob_3 = 52
+        Knob_4 = 53
+        Knob_5 = 54
+        Knob_6 = 55
+        Knob_7 = 56
+        Knob_8 = 29
+        Knob_9 = 30
+        Knob_10 = 31
+        Knob_11 = 32
+        Knob_12 = 33
+        Knob_13 = 34
+        Knob_14 = 35
+        Knob_15 = 36
+        Knob_16 = 13
+        Knob_17 = 14
+        Knob_18 = 15
+        Knob_19 = 16
+        Knob_20 = 17
+        Knob_21 = 18
+        Knob_22 = 19
+        Knob_23 = 20
         Fader_0 = 77
         Fader_1 = 78
         Fader_2 = 79
@@ -159,22 +159,22 @@ class XL(PyxelWidgets.Controllers.MIDI.MIDI):
         Factory_7 = 15
     
     colors = [0x0C, 0x0D, 0x0F, 0x1D, 0x3F, 0x3E, 0x1C, 0x3C]
-    pads = [[0x49, 0x29],
-            [0x4A, 0x2A],
-            [0x4B, 0x2B],
-            [0x4C, 0x2C],
-            [0x59, 0x39],
-            [0x5A, 0x3A],
-            [0x5B, 0x3B],
-            [0x5C, 0x3C]]
-    knobs = [[0x0F, 0x0E, 0x0D],
-             [0x1F, 0x1E, 0x1D],
-             [0x2F, 0x2E, 0x2D],
-             [0x3F, 0x3E, 0x3D],
-             [0x4F, 0x4E, 0x4D],
-             [0x5F, 0x5E, 0x5D],
-             [0x6F, 0x6E, 0x6D],
-             [0x7F, 0x7E, 0x7D]]
+    leds = [[0x49, 0x29, 0x0F, 0x0E, 0x0D],
+            [0x4A, 0x2A, 0x1F, 0x1E, 0x1D],
+            [0x4B, 0x2B, 0x2F, 0x2E, 0x2D],
+            [0x4C, 0x2C, 0x3F, 0x3E, 0x3D],
+            [0x59, 0x39, 0x4F, 0x4E, 0x4D],
+            [0x5A, 0x3A, 0x5F, 0x5E, 0x5D],
+            [0x5B, 0x3B, 0x6F, 0x6E, 0x6D],
+            [0x5C, 0x3C, 0x7F, 0x7E, 0x7D]]
+    ledsSysex = [[0x20, 0x18, 0x10, 0x08, 0x00],
+                 [0x21, 0x19, 0x11, 0x09, 0x01],
+                 [0x22, 0x1A, 0x12, 0x0A, 0x02],
+                 [0x23, 0x1B, 0x13, 0x0B, 0x03],
+                 [0x24, 0x1C, 0x14, 0x0C, 0x04],
+                 [0x25, 0x1D, 0x15, 0x0D, 0x05],
+                 [0x26, 0x1E, 0x16, 0x0E, 0x06],
+                 [0x27, 0x1F, 0x17, 0x0F, 0x07]]
 
     """ 
     Controller Class for Launchpad, Launchpad S, Launchpad Mini, Launchpad Mini MK2
@@ -194,13 +194,11 @@ class XL(PyxelWidgets.Controllers.MIDI.MIDI):
         self.currentTemplate = template
 
     def sendPixel(self, x: int, y: int, pixel: PyxelWidgets.Helpers.Pixel):
+        colorIndex = pixel.mono // 32
         if self.currentTemplate == XL.Templates.Factory_7:
-            colorIndex = pixel.gmono // 32
-            if y < 2:
-                self.sendNoteOn(XL.pads[x][y], XL.colors[colorIndex], 0xF)
-            else:
-                self.sendNoteOn(XL.knobs[x][y - 2], XL.colors[colorIndex], 0xF)
-            #self.sendSysex([0x00, 0x20, 0x29, 0x02, 0x11, 0x78, y, x, XL.colors[colorIndex]])
+            self.sendNoteOn(XL.leds[x][y], XL.colors[colorIndex], 0xF)
+        else:
+            self.sendSysex([0x00, 0x20, 0x29, 0x02, 0x11, 0x78, 0xF, XL.ledsSysex[x][y], XL.colors[colorIndex]])
 
     def connect(self, inPort: str = None, outPort: str = None):
         super().connect(inPort=inPort, outPort=outPort)
@@ -208,6 +206,7 @@ class XL(PyxelWidgets.Controllers.MIDI.MIDI):
         self.reset(self.currentTemplate)
 
     def disconnect(self):
+        self.reset(XL.Templates.Factory_7)
         self.selectTemplate(XL.Templates.Factory_0)
         super().disconnect()
 
@@ -222,19 +221,19 @@ class XL(PyxelWidgets.Controllers.MIDI.MIDI):
             if cmd == 0x80 or cmd == 0x90:
                 if midi[1] < 0x30:
                     x = midi[1] - 0x29
-                    y = 0
+                    y = 1
                     self.setButton(x, y, midi[2] / 127.0)
                 elif midi[1] < 0x40:
                     x = midi[1] - 0x35
-                    y = 0
+                    y = 1
                     self.setButton(x, y, midi[2] / 127.0)
                 elif midi[1] < 0x50:
                     x = midi[1] - 0x49
-                    y = 1
+                    y = 0
                     self.setButton(x, y, midi[2] / 127.0)
                 elif midi[1] < 0x60:
                     x = midi[1] - 0x55
-                    y = 1
+                    y = 0
                     self.setButton(x, y, midi[2] / 127.0)
                 else:
                     button = XL.Buttons(midi[1])
@@ -245,7 +244,7 @@ class XL(PyxelWidgets.Controllers.MIDI.MIDI):
             elif cmd == 0xB0:
                 control = XL.Controls(midi[1])
                 if midi[1] < 0x64:
-                    self.setCustom(PyxelWidgets.Helpers.Event.Changed, control.name, midi[2] / 127.0)
+                    self.setCustom(PyxelWidgets.Helpers.Event.Changed, control.name, midi[2] / 128.0)
                 else:
                     if midi[2] > 0:
                         self.setCustom(PyxelWidgets.Helpers.Event.Pressed, control.name, midi[2] / 127.0)
